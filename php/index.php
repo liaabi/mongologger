329cd5b559ad5318feb92f1bb25ff199c251034c
Ligia Arghir <ligia.arghir@gmail.com>
2014-07-14 10:36:22 +0200

    try again

--- php/index.php
+++ php/index.php
@@ -53,20 +53,20 @@ $resarray   = iterator_to_array($cursor);
            </tr>
 
 
-foreach ($resarray as $val) {
-   $id = $val["id"]
-   echo "<br><p>loading item #" . $id . "...\n";
-   $entry = convertToArray($val);
+//foreach ($resarray as $val) {
+//   $id = $val["id"]
+//   echo "<br><p>loading item #" . $id . "...\n";
+//   $entry = convertToArray($val);
 
-   #echo "<tr id='tweetrow'>\n";
-   #echo "  <td class='when' colspan='2'" . "'>" .  $entry['timestamp'] .
-   #     "  </td>\n";
-   #echo "  <td class='tag'>" .  $entry['tag'] .
-   #     "  </td>\n"
-   #echo "  <td class='who'>" .  $entry['host'] .
-   #     "  </td>\n";
-   #echo "</tr>\n";
-#}
+   //echo "<tr id='tweetrow'>\n";
+   //echo "  <td class='when' colspan='2'" . "'>" .  $entry['timestamp'] .
+   //     "  </td>\n";
+   //echo "  <td class='tag'>" .  $entry['tag'] .
+   //     "  </td>\n"
+   //echo "  <td class='who'>" .  $entry['host'] .
+   //     "  </td>\n";
+   //echo "</tr>\n";
+//}
 
 ?>
 
